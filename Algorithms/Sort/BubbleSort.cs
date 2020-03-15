2330010945edd529a040365ae9dd74a5a137efac
Ilyas <astarion@protonmail.com>
2020-03-15 16:05:25 +0600

    Added bubble sort

---
+++ Algorithms/Sort/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sort
+{
+    public class BubbleSort
+    {
+        public static void Sort(List<int> ListToSort)
+        {
+            for (int write = 0; write < ListToSort.Count; write++)
+            {
+                int temp = 0;
+                for (int sort = 0; sort < ListToSort.Count - 1; sort++)
+                {
+                    if (ListToSort[sort] > ListToSort[sort + 1])
+                    {
+                        temp = ListToSort[sort + 1];
+                        ListToSort[sort + 1] = ListToSort[sort];
+                        ListToSort[sort] = temp;
+                    }
+                }
+            }
+
+
+            foreach (int Sorted in ListToSort)
+            {
+                Console.Write($"{Sorted} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
